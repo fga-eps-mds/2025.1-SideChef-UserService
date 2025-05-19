b3713cc54aedac7cb23e51ea6278e76c61c49190
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2025-05-18 21:20:24 -0300

    Fix: Duplicata de ModelUser

--- user/models/user.py
+++ user/models/user.py
@@ -8,9 +8,3 @@ class User(Base):
     name = Column(String, nullable=False)
 
 
-class User(Base):
-    __tablename__ = 'users'
-
-    id: Mapped[int] = mapped_column(Integer, primary_key=True)
-    username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
-    created_at: Mapped[DateTime] = mapped_column(DateTime, default=datetime.now(timezone.utc), nullable=False)
