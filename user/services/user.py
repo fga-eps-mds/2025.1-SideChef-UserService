df4db6052e8ac62f199b194b70cc889cf904e2f5
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2025-06-07 22:19:24 -0300

    "Feat: Adding the Delete Metohd for Users."

--- user/services/user.py
+++ user/services/user.py
@@ -1,7 +1,8 @@
 from sqlalchemy.orm import Session
 from user.models.user import User
 from user.schemas.user import UserCreate
-
+from fastapi import HTTPException
+import traceback
 
 def create_user(db: Session, user: UserCreate) -> User:
     new_user = User(name=user.name, password=user.password, email=user.email, cpf=user.cpf)
@@ -12,3 +13,11 @@ def create_user(db: Session, user: UserCreate) -> User:
     
 def get_all_users(db: Session):
     return db.query(User).all()
+
+def delete_user(db: Session, user_id: int) -> bool:
+    user = db.query(User).filter(User.id == user_id).first()
+    if user:
+        db.delete(user)
+        db.commit()
+        return True
+    return False
