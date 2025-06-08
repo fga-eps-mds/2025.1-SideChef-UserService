3f9add9f3b02e8554cb5140b3c5c20a820e8890b
Pietrocvisentin@gmail.com <Pietrocvisentin@gmail.com>
2025-06-07 23:46:17 -0300

    "Feat: Users are know more safe, passwords are Hash on DB."

--- user/services/user.py
+++ user/services/user.py
@@ -2,14 +2,27 @@ from sqlalchemy.orm import Session
 from user.models.user import User
 from user.schemas.user import UserCreate
 from fastapi import HTTPException
+from sqlalchemy.exc import IntegrityError
+from core.security import gerar_hash_senha
 import traceback
 
 def create_user(db: Session, user: UserCreate) -> User:
-    new_user = User(name=user.name, password=user.password, email=user.email, cpf=user.cpf)
+    senha_criptografada = gerar_hash_senha(user.password)
+    new_user = User(
+        name=user.name,
+        password=senha_criptografada,
+        email=user.email,
+        cpf=user.cpf
+    )
+
     db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    return new_user
+    try:
+        db.commit()
+        db.refresh(new_user)
+        return new_user
+    except IntegrityError:
+        db.rollback()
+        raise HTTPException(status_code=409, detail="E-mail ou CPF já cadastrado.")
     
 def get_all_users(db: Session):
     return db.query(User).all()
